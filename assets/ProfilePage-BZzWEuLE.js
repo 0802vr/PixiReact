e220f226c76a31743e73f04fcc829a288504f6de
Василий Ровнов <vasyarovnov0802@gmail.com>
2024-12-03 13:35:08 +0400

    Updates

--- assets/ProfilePage-BZzWEuLE.js
+++
@@ -1,2 +0,0 @@
-import{r as e,j as r}from"./index-Ba5hMq8e.js";import{P as s}from"./ProfileNav-BhDX3yIz.js";function t(){return e.useEffect(()=>{window.scrollTo(0,0)},[]),r.jsx("main",{className:"",style:{padding:"200px 0"},children:r.jsx(r.Fragment,{children:r.jsx(s,{})})})}export{t as default};
-//# sourceMappingURL=ProfilePage-BZzWEuLE.js.map
