a8af5fe3766b617a049564b3ecb8ec96b86a52d0
Василий Ровнов <vasyarovnov0802@gmail.com>
2024-12-09 13:43:39 +0400

    Updates

--- assets/ProfilePage-85rDbHnK.js
+++
@@ -1,2 +0,0 @@
-import{r as s,j as r}from"./index-2lVvXSHG.js";import{P as e}from"./ProfileNav-DmN-h8Li.js";function t(){return s.useEffect(()=>{window.scrollTo(0,0)},[]),r.jsx("main",{className:"",style:{padding:"200px 0"},children:r.jsx(e,{})})}export{t as default};
-//# sourceMappingURL=ProfilePage-85rDbHnK.js.map
