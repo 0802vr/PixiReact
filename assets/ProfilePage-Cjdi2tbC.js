188e9d76c190c9146c03ad6dda8605282c8ccdea
Василий Ровнов <vasyarovnov0802@gmail.com>
2024-12-09 17:05:19 +0400

    Updates

--- assets/ProfilePage-Cjdi2tbC.js
+++
@@ -1,2 +0,0 @@
-import{r as s,j as r}from"./index-DKbGor7I.js";import{P as e}from"./ProfileNav-DGkgVHbB.js";function t(){return s.useEffect(()=>{window.scrollTo(0,0)},[]),r.jsx("main",{className:"",style:{padding:"200px 0"},children:r.jsx(e,{})})}export{t as default};
-//# sourceMappingURL=ProfilePage-Cjdi2tbC.js.map
