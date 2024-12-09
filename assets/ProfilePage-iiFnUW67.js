c486fcff6a437e447f94f8255ccdca735043bec8
Василий Ровнов <vasyarovnov0802@gmail.com>
2024-12-09 13:41:00 +0400

    Updates

--- assets/ProfilePage-iiFnUW67.js
+++
@@ -1,2 +0,0 @@
-import{r as s,j as r}from"./index-GZyK8iEE.js";import{P as e}from"./ProfileNav-DJXU6qtR.js";function t(){return s.useEffect(()=>{window.scrollTo(0,0)},[]),r.jsx("main",{className:"",style:{padding:"200px 0"},children:r.jsx(e,{})})}export{t as default};
-//# sourceMappingURL=ProfilePage-iiFnUW67.js.map
