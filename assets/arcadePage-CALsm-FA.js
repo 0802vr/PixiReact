1a85636a0794f18baf3bbacee1987fc5038232aa
Василий Ровнов <vasyarovnov0802@gmail.com>
2024-12-12 18:43:50 +0400

    Updates

---
+++ assets/arcadePage-CALsm-FA.js
@@ -0,0 +1,2 @@
+import{j as r}from"./index-DaVgEkjO.js";import{P as a}from"./ProfileNav-DRc3YxLc.js";import{A as e}from"./ArcadeBanner-CYLcV12-.js";const m=()=>r.jsxs(r.Fragment,{children:[r.jsx(a,{}),r.jsx(e,{})]});export{m as default};
+//# sourceMappingURL=arcadePage-CALsm-FA.js.map
