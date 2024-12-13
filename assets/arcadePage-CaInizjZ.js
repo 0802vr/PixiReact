64a0ba79e3c972461c4d3c15f9448c74144890fe
Василий Ровнов <vasyarovnov0802@gmail.com>
2024-12-13 16:29:05 +0400

    Updates

--- assets/arcadePage-CaInizjZ.js
+++
@@ -1,2 +0,0 @@
-import{j as a}from"./index-BpXeBJTa.js";import{P as r}from"./ProfileNav-DUEU25Pr.js";import{A as e}from"./ArcadeBanner-X3UnE8Pt.js";const t=()=>a.jsxs("main",{className:"mainArcadePage",children:[a.jsx(r,{}),a.jsx(e,{})]});export{t as default};
-//# sourceMappingURL=arcadePage-CaInizjZ.js.map
