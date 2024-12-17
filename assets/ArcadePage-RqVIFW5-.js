5020cd8f3bcf1df57ac79eac223abeaa9b2464c8
Василий Ровнов <vasyarovnov0802@gmail.com>
2024-12-17 16:49:04 +0400

    Updates

---
+++ assets/ArcadePage-RqVIFW5-.js
@@ -0,0 +1,2 @@
+import{j as a}from"./index-BqPe9ath.js";import{P as r}from"./ProfileNav-C3aVoYnR.js";import{A as e}from"./ArcadeBanner-DuCAtn1z.js";function i(){return a.jsxs("main",{className:"mainArcadePage",children:[a.jsx(r,{}),a.jsx(e,{})]})}export{i as default};
+//# sourceMappingURL=ArcadePage-RqVIFW5-.js.map
--- assets/ArcadePage-mGopmP2b.js
+++
@@ -1,2 +0,0 @@
-import{j as a}from"./index-B3illGOq.js";import{P as r}from"./ProfileNav-JDiJiPKR.js";import{A as e}from"./ArcadeBanner-B1Gs4b3m.js";function i(){return a.jsxs("main",{className:"mainArcadePage",children:[a.jsx(r,{}),a.jsx(e,{})]})}export{i as default};
-//# sourceMappingURL=ArcadePage-mGopmP2b.js.map
